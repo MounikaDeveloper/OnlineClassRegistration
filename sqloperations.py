73dec2aece05247915fd0f47452188ae3d8d5186
MounikaDeveloper <57697586+MounikaDeveloper@users.noreply.github.com>
2020-08-06 13:18:11 +0530

    ok

--- sqloperations.py
+++ sqloperations.py
@@ -1,5 +1,5 @@
 import sqlite3 as sql
-conn=sql.connect("mounika.sqlite2")
+conn=sql.connect("DATABASE_URL")
 curs=conn.cursor()
 def createCourseTable():
     curs.execute("create table course(cno number primary key,course_name text,faculty_name text,class_date date,class_time text,fee real,duration number)")
